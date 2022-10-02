4510d58e5ce7d4d330c92a643d12d53e73af238e
Uriel Avalos <uri@frankandrobot.com>
2022-10-01 22:38:59 -0500

    build(go-channels): building with TS

--- packages/js-go-channels/src/index.test.ts
+++ packages/js-go-channels/src/index.test.ts
@@ -1,19 +1,3 @@
-/*
- * Copyright 2017  Uriel Avalos
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { newChannel, go, select, close, range } from "./index";
 
 const tick = async (timeoutCounts = 5) => {
@@ -458,7 +442,7 @@ describe("select", () => {
     await tick();
   });
 
-  test("select roundrobins with closed channels", async () => {
+  test("select round robin with closed channels", async () => {
     expect.assertions(2);
     const c1 = newChannel();
     const c2 = newChannel();
